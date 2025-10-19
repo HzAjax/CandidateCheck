a690ac60c5d43a3ea916c1ac9c4967c4b58d7459
HzAjax <danil3000200@mail.ru>
2025-10-19 10:45:33 +0400

    add application bootstrap

---
+++ VerifyHub/verification-service/src/main/java/ru/volodin/verification/VerificationApplication.java
@@ -0,0 +1,10 @@
+package ru.volodin.verification;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VerificationApplication.class, args);
+    }
+}
